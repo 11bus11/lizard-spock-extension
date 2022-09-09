f9ffa4932d52b75772d91cb2c7778775c94e3dcf
11bus11 <02linfor01@gmail.com>
2022-09-09 13:57:29 +0200

    declared variables for point-choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-function chosen(){
-    let compChoice = 
+function pointChoiceEvent() {
+    let threePoint = document.getElementById("3-point")
+    let fivePoint = document.getElementById("5-point")
+    let tenPoint = document.getElementById("10-point")
+    var pointChoice = 
 }

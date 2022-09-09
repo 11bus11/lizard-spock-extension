8c8fae115576eeecbe93520712d8d39169d168f0
11bus11 <02linfor01@gmail.com>
2022-09-09 23:48:52 +0200

    made point choice functions
    Made functions for when you choose how many points are needed to win. Depending on which one you choose the pointChoice variable gets a different value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
-function pointChoiceEvent() {
-    let threePoint = document.getElementById("3-point")
-    let fivePoint = document.getElementById("5-point")
-    let tenPoint = document.getElementById("10-point")
-    var pointChoice = 
-}
+var pointChoice =  0;
+
+let threePoint = document.getElementById("3-point");
+let fivePoint = document.getElementById("5-point");
+let tenPoint = document.getElementById("10-point");
+
+threePoint.addEventListener("click", function() {
+    pointChoice = 3;
+    console.log(pointChoice);
+});
+fivePoint.addEventListener("click", function() {
+    pointChoice = 5;
+    console.log(pointChoice);
+});
+tenPoint.addEventListener("click", function() {
+    pointChoice = 10;
+    console.log(pointChoice);
+});

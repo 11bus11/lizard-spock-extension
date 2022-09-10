bd0beff4b32acfd1fac090cc1318ba5d0add258a
11bus11 <02linfor01@gmail.com>
2022-09-10 20:21:54 +0200

    switch - playGame function
    made a switch with different outputs depending on which button you clicked on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,13 +46,33 @@ const choiceObjects = [
 ];
 
 let randomNumber = 0;
+
 function compChoiceRandom () {
     randomNumber = Math.floor(Math.random() * 5);
-    console.log(randomNumber)
 }
 
-function playGame(clicked) {
-    compChoiceRandom();
+let youChoice = "void"
 
-    console.log(clicked)
+function playGame(clicked) {
+    switch (clicked) {
+        case choiceObjects[0]:
+            console.log("rock");
+            break;
+        case choiceObjects[1]:
+            console.log("paper");
+            break;
+        case choiceObjects[2]:
+            console.log("scissors");
+            break;
+        case choiceObjects[3]:
+            console.log("lizard");
+            break;
+        case choiceObjects[4]:
+            console.log("spock");
+            break;
+                
+            
+    }
+
+    compChoiceRandom();
 }

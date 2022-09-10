58d719569bfd95d859c8c418ec0ab5d8ced23056
11bus11 <02linfor01@gmail.com>
2022-09-10 15:20:40 +0200

    made function for pointChoice, added objects
    put everything that happens when you choose how many points are needed into a function. Also created 2 objects (rock and paper) that i will hopefully be able to use for the game itself

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,34 @@
 var pointChoice = 0;
 
-var threePoint = document.getElementById("3-point");
-var fivePoint = document.getElementById("5-point");
-var tenPoint = document.getElementById("10-point");
+function choosePointAmount() {
+    const threePoint = document.getElementById("3-point");
+    const fivePoint = document.getElementById("5-point");
+    const tenPoint = document.getElementById("10-point");
 
-threePoint.addEventListener("click", function() {
-    pointChoice = 3;
-    console.log(pointChoice);
-});
-fivePoint.addEventListener("click", function() {
-    pointChoice = 5;
-    console.log(pointChoice);
-});
-tenPoint.addEventListener("click", function() {
-    pointChoice = 10;
-    console.log(pointChoice);
-});
+    threePoint.addEventListener("click", function() {
+        pointChoice = 3;
+        console.log(pointChoice);
+    });
+    fivePoint.addEventListener("click", function() {
+        pointChoice = 5;
+        console.log(pointChoice);
+    });
+    tenPoint.addEventListener("click", function() {
+        pointChoice = 10;
+        console.log(pointChoice);
+    });
+    return pointChoice;
+};
 
-console.log(pointChoice);
+
+
+const choiceObjects = [
+    {
+        name: "rock",
+        beats: ["scissors", "lizard"]
+    },
+    {
+        name: "paper",
+        beats: ["rock", "spock"]
+    }
+];
